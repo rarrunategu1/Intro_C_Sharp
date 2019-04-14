b8bb11f540d631239907e7d4aebceed8365a2d64
rarrunategu1 <parker.rose@ymail.com>
2019-04-14 18:29:07 -0400

    Simple program to check if user input is a number between 1 and 10

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,18 +1,6 @@
 ﻿//namespace in .NET framework, where we have basic utility classes and primitive typster
 using System;
 
-//used to work with lists, collections
-using System.Collections.Generic;
-
-//used to work with data
-using System.Linq;
-
-//used to work with text and coding
-using System.Text;
-
-//used to build multi threaded applications
-using System.Threading.Tasks;
-
 namespace HelloWorld
 {
     class Program
@@ -20,7 +8,21 @@ namespace HelloWorld
         //entry point to app
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Please enter a number: ");
+
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (userNumber > 0 && userNumber < 11)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            Console.ReadLine();
         }
+
     }
 }
