0277246c2ca741edc816f61ce07a37e992dd1bf9
rarrunategu1 <parker.rose@ymail.com>
2019-04-15 20:01:10 -0400

    program takes the a number as an input and then returns the factorial of that number

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,26 +1,31 @@
 ﻿//namespace in .NET framework, where we have basic utility classes and primitive typster
 using System;
 
-//used to work with lists, collections
-using System.Collections.Generic;
-
-//used to work with data
-using System.Linq;
-
-//used to work with text and coding
-using System.Text;
-
-//used to build multi threaded applications
-using System.Threading.Tasks;
-
 namespace HelloWorld
 {
     class Program
     {   
-        //entry point to app
+
+        public static long factorialize(int number)
+        {   
+           long factorial = 1;
+            
+           while (number > 1)
+           {
+                factorial *= number;
+                number--;
+           }
+           return factorial;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Please enter a number to factorialize:  ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+           
+            long result = factorialize(number);
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
