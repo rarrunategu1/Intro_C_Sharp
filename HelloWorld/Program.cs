5aec0f5b72f1b1215485f107dbc9ae7961f9d996
rarrunategu1 <parker.rose@ymail.com>
2019-04-15 20:02:21 -0400

    removed some comments

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,18 +1,6 @@
 ﻿//namespace in .NET framework, where we have basic utility classes and primitive typster
 using System;
 
-//used to work with lists, collections
-using System.Collections.Generic;
-
-//used to work with data
-using System.Linq;
-
-//used to work with text and coding
-using System.Text;
-
-//used to build multi threaded applications
-using System.Threading.Tasks;
-
 namespace HelloWorld
 {
     class Program
