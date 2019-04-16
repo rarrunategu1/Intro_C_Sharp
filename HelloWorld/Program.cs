7c092fa457bbd4fcf14e9600ae240de0a8cace09
rarrunategu1 <parker.rose@ymail.com>
2019-04-15 21:03:44 -0400

    program that gives a user 4 tries to guess a secret number that is randomly picked.  if they guess right it will print that they won or else they'll print that they lost

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,10 +5,46 @@ namespace HelloWorld
 {
     class Program
     {   
-        //entry point to app
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
+        {   
+            var random = new Random();
+            int secretNumber = 0;
+            
+            for (int numberLength = 1; numberLength < 2; numberLength++)
+            {
+                secretNumber += (random.Next(1, 10));
+                Console.WriteLine("Secret number is {0}",secretNumber);
+                Console.WriteLine();
+            }
+
+            int guessTries = 4;
+
+            while (guessTries > 0)
+            {
+            
+                Console.WriteLine("Guess the secret number: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                guessTries--;
+
+                Console.WriteLine();
+
+                if (guess == secretNumber)
+                {
+                    Console.WriteLine("You won");
+                    Console.ReadLine();
+                    break;
+                }
+                else if (guess != secretNumber)
+                {
+                    Console.WriteLine("You have {0} more guesses", guessTries);  
+                    
+                    if (guessTries <  1 && guess != secretNumber)
+                    {
+                        Console.WriteLine("You lost");
+                        Console.ReadLine();
+                    }
+                }
+            }
         }
     }
 }
