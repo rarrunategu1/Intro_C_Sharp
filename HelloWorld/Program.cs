94ea795a8f45175621bce96cce5dbbc1bafe679d
rarrunategu1 <parker.rose@ymail.com>
2019-04-17 19:05:46 -0400

    Added strings method that goes over string.Format, string.Join and verbatimText

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,8 +11,11 @@ namespace HelloWorld
             Console.WriteLine("variables method: {0}",constVariable);
             Console.WriteLine();
 
-
             typeConversion();
+            Console.WriteLine();
+
+            strings();
+            Console.WriteLine();
 
 
             Console.ReadLine();
@@ -32,13 +35,41 @@ namespace HelloWorld
                 string trueOrFalse = "Hi";
                 bool didStringSayTrueOrFalse = Convert.ToBoolean(trueOrFalse);
                 Console.WriteLine("typeConversion Method: " +didStringSayTrueOrFalse);
-                Console.ReadLine();
             }
             catch (Exception)
             {
                 Console.WriteLine("typeConversion Method:  The string could not be converted into a boolean");
-                Console.ReadLine();
             }
+        }
+
+        public static void strings()
+        {   
+            Console.WriteLine("strings Method:");
+            string firstName = "Rose";
+            string lastName  = "Smith";
+
+            string fullName = firstName + " " + lastName;
+
+            //String.Format
+            string myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+            Console.WriteLine("string.format: " +myFullName);
+
+            //String.Join
+            string[] names = new string[3] {"Bill", "Suzy", "Tony"};
+            string formatedNames = string.Join(",", names);
+            Console.WriteLine("string.Join: " +formatedNames);
+
+            //Verbatim String
+            string text = "Hi Linda\nPlease see my email with the following link to paths\nc:\\folder1\\folder2\nc:\\folder3\\folder4";
+            
+            string verbatimText = @"Hi Linda
+            Please see my email with the following link to paths
+            c:\folder1\folder2
+            c:\folder3\folder4";
+            
+            Console.WriteLine("verbatimText: " +verbatimText);
+            Console.ReadLine();
+
         }
     }
 
